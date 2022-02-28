ff21751d890e1967c64da26d59d6503dc49b56ef
omar <elewa97@aucegypt.edu>
2022-02-28 14:22:51 +0200

    Routes set

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import GlobalStyles from "./globalStyles";
+import {ThemeProvider} from "styled-components";
+import {lightTheme} from "./components/Themes";
+import {Route, Switch} from "react-router-dom";
+
+// Components
+import Main from "./components/Main"
+import AboutPage from "./components/AboutPage"
+import BlogPage from "./components/BlogPage"
+import MySkillsPage from "./components/MySkillsPage"
+import WorkPage from "./components/WorkPage"
+
 function App() {
-  return <div>
-    App File
-    </div>
+  return <>
+    <GlobalStyles/>
+    <ThemeProvider theme={lightTheme}>
+      <Switch>
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/about" component={AboutPage}/>
+        <Route exact path="/blog" component={BlogPage}/>
+        <Route exact path="/work" component={WorkPage}/>
+        <Route exact path="/skills" component={MySkillsPage}/>
+      </Switch>
+    </ThemeProvider>
+    </>
     
 }
 
